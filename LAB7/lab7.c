44136f78fde8b32f33849d4af9dc89f41a1537e9
folkmore <folkmore@163.com>
2024-05-21 16:32:56 +0800

    ?

--- LAB7/lab7.c
+++ LAB7/lab7.c
@@ -1,45 +1,3 @@
-//做了一些没用的改动
-//***************************************************************************//
-//                                                                           //
-//                         MSP-EXP430F5529 LAB CODE                          //
-//                                                                           //
-//                            lab7 - ComplexLAB                              //
-//                                                                           //
-//***************************************************************************//
-/*******************************************************************************
- *
- *  Copyright (C) 2012 Texas Instruments Incorporated - http://www.ti.com/
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *    Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- *    Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the
- *    distribution.
- *
- *    Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************/
-
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -388,6 +346,7 @@ void UpdateMines(void)
 {
     uint8_t count, max;
     uint32_t idx;
+
     uint8_t dir =1;
     uint8_t mineoffset = 0;
 
@@ -417,7 +376,7 @@ void UpdateMines(void)
         	//dir = -dir;
         //}
 
-        mines[count][2]--;
+        // mines[count][2]--;
 
         // If the mine is too far off the left edge of the display then disable it.
         if (mines[count][1] == -8)
